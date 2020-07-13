88edf2ab3aafa46d5f89f33f0c2837f5f857cf62
Bharath Ballipongala <bharathb294@gmail.com>
2020-07-12 23:46:59 -0400

    Intial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace nightshark.github.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
